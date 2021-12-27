1de01571ba726859f99c8b5c6756209e88e1e44b
ELOY <theo.eloy@etud.u-picardie.fr>
2021-12-27 16:31:03 +0100

    Bases Horloge universelle

---
+++ src/main/java/horloge/HorlogeUniverselle.java
@@ -0,0 +1,51 @@
+package horloge;
+
+import java.util.Date;
+
+public class HorlogeUniverselle{
+    private int joursParSeconde;
+    private Date dateSimu;
+    private Date dateReel;
+
+    public HorlogeUniverselle(int joursParSeconde) {
+        this.joursParSeconde = joursParSeconde;
+        dateSimu = new Date(System.currentTimeMillis());
+        dateReel = new Date(System.currentTimeMillis());
+    }
+
+    public HorlogeUniverselle(){
+        this(15);
+    }
+
+    @Override
+    public String toString() {
+        return "HorlogeUniverselle{" +
+                "joursParSeconde=" + joursParSeconde +
+                ", date=" + dateSimu +
+                ", instant=" + dateReel +
+                '}';
+    }
+
+    public int getJoursParSeconde() {
+        return joursParSeconde;
+    }
+
+    public Date getDateSimu() {
+        return dateSimu;
+    }
+
+    public Date getDateReel() {
+        return dateReel;
+    }
+
+    public void majHorloge(){
+        Date dateReel2 = new Date(System.currentTimeMillis());
+
+        if(dateReel2.after(dateReel)){
+            dateSimu.setDate((int)
+                    (dateSimu.getDate()+(
+                            joursParSeconde*(dateReel2.getTime()-dateReel.getTime())/1000)
+                    ));
+        }
+    }
+}
