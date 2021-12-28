13ec499c6a62f953c2e707fb24bf8af834b2900c
ELOY <theo.eloy@etud.u-picardie.fr>
2021-12-28 18:03:42 +0100

    Ajustement angleRotation pour préparer utilisation HorlogeUniverselle

--- src/main/java/Systeme/UseSysteme.java
+++ src/main/java/Systeme/UseSysteme.java
@@ -22,14 +22,14 @@ public class UseSysteme {
 
         planete soleil = new planete(Color.orange);
 
-        planete mercure = new planete(0.57 + soleil.getRayon(), 0.5, 0.005, 4.147 * rotationTerre, Color.gray); //88 jours
-        planete venus = new planete(0.64 + soleil.getRayon(), 0.5, 0.01, 1.629 * rotationTerre, Color.pink);// 224 jours
-        planete terre = new planete(0.7 + soleil.getRayon(), 0.5, 0.02, rotationTerre, Color.green); // 365 jours
-        planete mars = new planete(0.8 + soleil.getRayon(), 0.5, 0.007, 0.531 * rotationTerre, 247, 111, 82); // 687 jours
-        planete jupiter = new planete(1.54 + soleil.getRayon(), 0.5, 0.03, 0.084 * rotationTerre, 217, 174, 81); // 4 332 jours
-        planete saturne = new planete(2.41 + soleil.getRayon(), 0.5, 0.028, 0.033 * rotationTerre, 225, 199, 117); // 10 759 jours
-        planete uranus = new planete(4.33 + soleil.getRayon(), 0.5, 0.023, 0.011 * rotationTerre, Color.white); // 30 685 jours
-        planete neptune = new planete(6.5 + soleil.getRayon(), 0.5, 0.0235, 0.006 * rotationTerre, Color.blue); // 60 216 jours
+        planete mercure = new planete(0.57 + soleil.getRayon(), 0.5, 0.005, 360/87.96, Color.gray); //88 jours
+        planete venus = new planete(0.64 + soleil.getRayon(), 0.5, 0.01, 360/224.69, Color.pink);// 224 jours
+        planete terre = new planete(0.7 + soleil.getRayon(), 0.5, 0.02, 360/365.25, Color.green); // 365 jours
+        planete mars = new planete(0.8 + soleil.getRayon(), 0.5, 0.007, 360/686.97, 247, 111, 82); // 687 jours
+        planete jupiter = new planete(1.54 + soleil.getRayon(), 0.5, 0.03, 360/4332.589, 217, 174, 81); // 4 332 jours
+        planete saturne = new planete(2.41 + soleil.getRayon(), 0.5, 0.028, 360/10759.23, 225, 199, 117); // 10 759 jours
+        planete uranus = new planete(4.33 + soleil.getRayon(), 0.5, 0.023, 360/30685.4, Color.white); // 30 685 jours
+        planete neptune = new planete(6.5 + soleil.getRayon(), 0.5, 0.0235, 360/60216.8, Color.blue); // 60 216 jours
 
         List<planete> planetes;
         planetes = Arrays.asList(soleil, mercure, venus, terre, mars, jupiter, saturne, uranus, neptune);
@@ -42,7 +42,6 @@ public class UseSysteme {
         while(f.isContinu()){
             f.affiche(planetes);
         }
-
 
 
     }
