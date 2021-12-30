86f2f41a85571ba2455f18e763e5eabaa9701694
ELOY <theo.eloy@etud.u-picardie.fr>
2021-12-30 17:12:15 +0100

    Changement couleurs planètes et séparation des planètes qui se superposent

--- src/main/java/Systeme/UseSysteme.java
+++ src/main/java/Systeme/UseSysteme.java
@@ -22,20 +22,20 @@ public class UseSysteme {
 
 
         //Constante rotationTerre (toutes les autres planètes se déplacent autour du soleil en fonction de la période de révolution de la Terre)
-        final double rotationTerre = 0.02;
+        final double rotationTerre = 0.0015;
 
         //Création des planetes
 
         planete soleil = new planete(Color.orange);
 
-        planete mercure = new planete(0.6, 0.5, 0.005, 4.147 * rotationTerre, Color.gray); //88 jours
-        planete venus = new planete(0.65, 0.5, 0.01, 1.629 * rotationTerre, Color.pink);// 224 jours
-        planete terre = new planete(0.70, 0.5, 0.02, rotationTerre, Color.green); // 365 jours
-        planete mars = new planete(0.75, 0.5, 0.007, 0.531 * rotationTerre, 247, 111, 82); // 687 jours
-        planete jupiter = new planete(0.8, 0.5, 0.03, 0.084 * rotationTerre, 217, 174, 81); // 4 332 jours
-        planete saturne = new planete(0.85, 0.5, 0.028, 0.033 * rotationTerre, 225, 199, 117); // 10 759 jours
-        planete uranus = new planete(0.90, 0.5, 0.023, 0.011 * rotationTerre, Color.white); // 30 685 jours
-        planete neptune = new planete(0.95, 0.5, 0.0235, 0.006 * rotationTerre, Color.blue); // 60 216 jours
+        planete mercure = new planete(0.6, 0.5, 0.005, 4.147 * rotationTerre, 191,179,164); //88 jours
+        planete venus = new planete(0.65, 0.5, 0.01, 1.629 * rotationTerre, 221,216,212);// 224 jours
+        planete terre = new planete(0.70, 0.5, 0.02, rotationTerre, Color.blue); // 365 jours
+        planete mars = new planete(0.75, 0.5, 0.007, 0.531 * rotationTerre, 178, 46, 32); // 687 jours
+        planete jupiter = new planete(0.8, 0.5, 0.03, 0.084 * rotationTerre, 235, 160, 116); // 4 332 jours
+        planete saturne = new planete(0.86, 0.5, 0.028, 0.033 * rotationTerre, 214, 182, 117); // 10 759 jours
+        planete uranus = new planete(0.92, 0.5, 0.023, 0.011 * rotationTerre, 190,225,229); // 30 685 jours
+        planete neptune = new planete(0.97, 0.5, 0.0235, 0.006 * rotationTerre, 84,138,232); // 60 216 jours
 
         List<planete> planetes;
         planetes = Arrays.asList(soleil, mercure, venus, terre, mars, jupiter, saturne, uranus, neptune);
@@ -50,13 +50,5 @@ public class UseSysteme {
         while(f.isContinu()){
             f.affiche(planetes, horloge);
         }
-
-        try {
-            Thread.sleep(36,5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("aaa");
     }
 }
